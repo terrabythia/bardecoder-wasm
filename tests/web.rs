1d54402ba437a82b9c8803eee848f7e9c50197db
Sander Bruggeman <sander.bruggeman@greenberry.nl>
2023-03-17 15:26:30 +0100

    remove unused js-sys crate

--- tests/web.rs
+++ tests/web.rs
@@ -5,18 +5,7 @@
 extern crate wasm_bindgen_test;
 use bardecoder_wasm::*;
 use wasm_bindgen::prelude::*;
-use wasm_bindgen_futures::JsFuture;
 use wasm_bindgen_test::*;
-
-#[wasm_bindgen_test]
-async fn my_async_test() {
-    // Create a promise that is ready on the next tick of the micro task queue.
-    let promise = js_sys::Promise::resolve(&JsValue::from(42));
-
-    // Convert that promise into a future and make the test wait on it.
-    let x = JsFuture::from(promise).await.unwrap();
-    assert_eq!(x, 42);
-}
 
 #[wasm_bindgen_test]
 async fn async_decoder_test() {
